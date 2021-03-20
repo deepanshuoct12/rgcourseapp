bc62da954be662f45730165c2178c6ee7f30a72d
Deepanshu <deepanshu.oct12@gmail.com>
2021-03-20 14:06:29 +0530

    dummy contact page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,5 @@
 import * as React from "react"
 import { graphql } from 'gatsby'
-import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HeroSection from "../components/Reuseable/HeroSection"
